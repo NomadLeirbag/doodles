9eacdd26bae56fe3b116733a7a046050a22c9b95
Gabriel Damon <gabrieldamon42@gmail.com>
2017-01-15 11:20:12 -0800

    starting to style the site

--- build.rb
+++ build.rb
@@ -1,10 +1,20 @@
 require "date"
 
-def pretty_dir(dir)
+def dir_parts(dir)
   parts = dir.split("_")
-  day = parts[0].split(/(\d)/)[1]
+  num = parts[0].split(/(\d)/)[1]
   month = Date::MONTHNAMES.select { |m| !m.nil? && m.downcase.include?(parts[1]) }[0]
-  "day #{day}: #{month} #{parts[2]}, 20#{parts[3]}"
+  { num: num, month: month, day: parts[2], year: "20#{parts[3]}" }
+end
+
+def pretty_text_dir(dir)
+  parts = dir_parts(dir)
+  "day #{parts[:num]}: #{parts[:month]} #{parts[:day]}, #{parts[:year]}"
+end
+
+def pretty_html_dir(dir)
+  parts = dir_parts(dir)
+  "<a href='#{dir}/index.html' class='list_item'><div class='num'>#{parts[:num]}</div><div class='date'>#{parts[:month]} #{parts[:day]}, #{parts[:year]}</div></a>"
 end
 
 File.open("index.html", "w") do |homepage|
@@ -16,9 +26,10 @@ File.open("index.html", "w") do |homepage|
   <head>
     <meta charset='UTF-8'>
     <title>daily doodles doubtfully dueling depression</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
-    <p>days: #{count}</p>
+    <p>#{count} daily doodles doubtfully dueling depression</p>
   eos
   dirs.each_with_index do |dir, i|
     File.open("#{dir}/index.html", "w") do |doodlepage|
@@ -28,16 +39,17 @@ File.open("index.html", "w") do |homepage|
   <head>
     <meta charset='UTF-8'>
     <title>daily doodles doubtfully dueling depression</title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
     <script src='../processing.min.js'></script>
   </head>
   <body>
-    <p>#{pretty_dir(dir)}</p>
+    <p>#{pretty_text_dir(dir)}</p>
       eos
-      doodlepage.write "    <p>" +
-        (i < count-1 ? "<a href='../#{dirs[i+1]}/index.html'>previous</a>" : "<s>previous</s>") +
-        " | <a href='../index.html'>list</a> | " +
-        (i > 0 ? "<a href='../#{dirs[i-1]}/index.html'>next</a>" : "<s>next</s>") +
-        "</p>\n"
+      doodlepage.write "    <div class='nav'>" +
+        (i < count-1 ? "<a href='../#{dirs[i+1]}/index.html' class='prev'>previous</a>" : "<s class='prev disabled'>previous</s>") +
+        "<a href='../index.html' class='list'>list</a>" +
+        (i > 0 ? "<a href='../#{dirs[i-1]}/index.html' class='next'>next</a>" : "<s class='next disabled'>next</s>") +
+        "</div>\n"
       doodlepage.write <<-eos
     <canvas data-processing-sources="#{dir}.pde"></canvas>
   </body>
@@ -45,7 +57,7 @@ File.open("index.html", "w") do |homepage|
       eos
     end
     homepage.write <<-eos
-    <p><a href='#{dir}/index.html'>#{pretty_dir(dir)}</a></p>
+    #{pretty_html_dir(dir)}
     eos
   end
   homepage.write <<-eos
