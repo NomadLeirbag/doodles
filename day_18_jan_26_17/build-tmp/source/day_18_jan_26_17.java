bf3fe9b538f5c539d00ab1662ea12403ada857bd
Gabriel Damon <gabrieldamon42@gmail.com>
2017-01-27 00:10:00 -0800

    whoops

--- day_18_jan_26_17/build-tmp/source/day_18_jan_26_17.java
+++ day_18_jan_26_17/build-tmp/source/day_18_jan_26_17.java
@@ -20,8 +20,8 @@ public void setup() {
   textSize(20);
   fill(0);
 
-  String lines[] =loadStrings(
-    "http://dailydigitaldoodlesdoubtfullyduelingdepression.gabriel-damon.com/day_18_jan_26_17/day_18_jan_26_17.pde");
+  String lines[] =loadStrings("http://dailydigitaldoodlesdoubtfullyduelingdepression" +
+                              ".gabriel-damon.com/day_18_jan_26_17/day_18_jan_26_17.pde");
   text("There are " + lines.length + " lines:", 20, 40);
   for(int i = 0 ; i < lines.length; i++) {
     text(lines[i], 20, 40+20*(i+2));
